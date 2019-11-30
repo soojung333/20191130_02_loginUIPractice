cf1ac466e673664478d5f97e1b3682f3cc1243da
blessme333 <blessme333@gmail.com>
2019-11-30 14:54:41 +0900

    Initial commit

---
+++ app/src/main/java/kr/co/tjoeun/a20191130_02_loginuipractice/MainActivity.kt
@@ -0,0 +1,12 @@
+package kr.co.tjoeun.a20191130_02_loginuipractice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
